34ad712d683d98029e25f310e4fb72b85ef6b9ac
Dan Gahan <dangahan@unknown.home>
2017-01-22 19:21:13 +0000

    NetScan completed (scheduler and logging added), DevInfo has scheduler and logging, needs DB inserts adding.

--- batch/DevInfo.py
+++ batch/DevInfo.py
@@ -12,6 +12,14 @@ from ConfigParser import SafeConfigParser
 import urllib2
 import json
 import codecs
+import sched
+import time
+import datetime
+import logging
+
+#Setup logging
+logging.basicConfig(filename='../logs/DevInfo.log',level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
 
 #Import varibales from NetMan.conf
 Config = SafeConfigParser()
@@ -22,106 +30,111 @@ CIDR =  Config.get('config', 'network')
 
 #Setup Database Connection
 try:
-            conn_string = "host='192.168.0.44' dbname='netman' user='netman' password='netman'"
+            conn_string = "host='localhost' dbname='netman' user='netman' password='netman'"
             conn = psycopg2.connect(conn_string)
-            print "SUCCESS: Database Connection Established"
-            print "\n"
+            logging.info('SUCCESS: Database Connection Established')
 except:
-            print "ERROR: I am unable to connect to the database"
+            logging.warning('ERROR: I am unable to connect to the database')
 
 cursor = conn.cursor()
 
-#Obtain all unique MAC address entries from DB and place into list, MAClistPre initially pulls all entries but with extract characters, MAClist then removes these characters
-cursor.execute('SELECT DISTINCT "MAC" from "NetScan";')
-MAClistPre = cursor.fetchall()
-MAClist = list(sum(MAClistPre, ()))
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc): 
+    s.enter(86400, 1, do_something, (sc,))
+
+    logging.info('SUCCESS: Scheduler starting')
+
+    #Obtain all unique MAC address entries from DB and place into list, MAClistPre initially pulls all entries but with extract characters, MAClist then removes these characters
+    cursor.execute('SELECT DISTINCT "MAC" from "NetScan";')
+    MAClistPre = cursor.fetchall()
+    MAClist = list(sum(MAClistPre, ()))
 
 
-#loop through MAClist and obtain info on each device. Insert info into database
-MAClistLength = len(MAClist)
-Counter = 0
+    #loop through MAClist and obtain info on each device. Insert info into database
+    MAClistLength = len(MAClist)
+    Counter = 0
 
-while (Counter < MAClistLength):
-    #Pull first MAC from the list
-    MAC = MAClist[Counter]
-    # Cast MAC as String
-    MAC = str(MAC)
-    print  "MAC Address: " + MAC
-    
+    while (Counter < MAClistLength):
+        #Pull first MAC from the list
+        MAC = MAClist[Counter]
+        # Cast MAC as String
+        MAC = str(MAC)
+        logging.debug('MAC Address: %s',  MAC)
+        
 
-    #Obtain latest IP assigned to MAC Address
-    IPextract_CMD = "/usr/sbin/arp -n | grep '" +  MAC + "' | awk '{print $1}'"
-    IPext_CMD_Output = subprocess.Popen([IPextract_CMD], shell=True, stdout=subprocess.PIPE).stdout
+        #Obtain latest IP assigned to MAC Address
+        IPextract_CMD = "/usr/sbin/arp -n | grep '" +  MAC + "' | awk '{print $1}'"
+        IPext_CMD_Output = subprocess.Popen([IPextract_CMD], shell=True, stdout=subprocess.PIPE).stdout
 
-    DevIP = IPext_CMD_Output.read().splitlines()
+        DevIP = IPext_CMD_Output.read().splitlines()
 
-    if len(DevIP) > 0:
-        DevIP = DevIP[0]
-    else:
-        DevIP = "UNKNOWN"
-    #DevIPList[0] = DevIP
-    print "IP: " + DevIP
-    
+        if len(DevIP) > 0:
+            DevIP = DevIP[0]
+        else:
+            DevIP = "UNKNOWN"
+        #DevIPList[0] = DevIP
+        logging.debug('IP: %s', DevIP)
+        
 
-    #Identify Device Hostname
-    DevHostname_CMD = "/usr/bin/nmap -sL  " + CIDR + "| grep -w " + DevIP + " | awk '{print $5}'"
-    DevHostname_CMD_Output = subprocess.Popen([DevHostname_CMD], shell=True, stdout=subprocess.PIPE).stdout
-    DevHostname = DevHostname_CMD_Output.read().splitlines()
+        #Identify Device Hostname
+        DevHostname_CMD = "/usr/bin/nmap -sL  " + CIDR + "| grep -w " + DevIP + " | awk '{print $5}'"
+        DevHostname_CMD_Output = subprocess.Popen([DevHostname_CMD], shell=True, stdout=subprocess.PIPE).stdout
+        DevHostname = DevHostname_CMD_Output.read().splitlines()
 
-    if len(DevHostname) > 0:
-        DevHostname = DevHostname[0]
-    else:
-        DevHostname = "UKNOWN"
-    print "Hostname: " + DevHostname
+        if len(DevHostname) > 0:
+            DevHostname = DevHostname[0]
+        else:
+            DevHostname = "UKNOWN"
+        logging.debug('Hostname: %s', DevHostname)
 
 
-    #Identify Device Operating System
-    DevOS_CMD = "/usr/bin/sudo /usr/bin/nmap -O -T5 " + DevIP + " | grep Running"
-    
-    if DevIP is "UNKNOWN":
-        DevOS = "UNKNOWN"
-    else:   
-        DevOS_CMD_Output = subprocess.Popen([DevOS_CMD], shell=True, stdout=subprocess.PIPE).stdout
-        DevOS = DevOS_CMD_Output.read().splitlines()
-
-    if isinstance(DevOS, list) and len(DevOS) > 0:
-             DevOS = DevOS[0]
-    else:
+        #Identify Device Operating System
+        DevOS_CMD = "/usr/bin/sudo /usr/bin/nmap -O -T5 " + DevIP + " | grep Running"
+        
+        if DevIP is "UNKNOWN":
             DevOS = "UNKNOWN"
-    
-    DevOS = str(DevOS)
-    print "OS: " + DevOS
+        else:   
+            DevOS_CMD_Output = subprocess.Popen([DevOS_CMD], shell=True, stdout=subprocess.PIPE).stdout
+            DevOS = DevOS_CMD_Output.read().splitlines()
 
-    
-    #Identify interface manufacturer from macvendors
-    url = "http://macvendors.co/api/"
-    request = urllib2.Request(url+MAC, headers={'User-Agent' : "API Browser"})
-    response = urllib2.urlopen( request )
-    reader = codecs.getreader("utf-8")
-    MACLookupResponse = json.load(reader(response))
-    #print type(MACLookupResponse)
-
-    if MACLookupResponse['result'].has_key('company'):
-        MACVendor =  MACLookupResponse['result']['company']
-    else:
-        MACVendor = "UNKNOWN"  
+        if isinstance(DevOS, list) and len(DevOS) > 0:
+                DevOS = DevOS[0]
+        else:
+                DevOS = "UNKNOWN"
         
-    print "Interface Vendor: " + MACVendor
-    
-    
-    #Identify Open Ports on Device
-    Scan_CMD = "sudo nmap -n -PN " + DevIP + " | grep -w open | awk '{print $1}'"
-    if DevIP is "UNKNOWN":
-        DevPorts = "UNKNOWN"
-    else:
-        Scan_CMD_Output = subprocess.Popen([Scan_CMD], shell=True, stdout=subprocess.PIPE).stdout
-        DevPorts = Scan_CMD_Output.read().splitlines()
-        DevPorts = '[%s]' % ', '.join(map(str, DevPorts))
+        DevOS = str(DevOS)
+        logging.debug('OS: %s', DevOS)
 
-    
-    print "Open Ports: " + DevPorts
         
+        #Identify interface manufacturer from macvendors
+        url = "http://macvendors.co/api/"
+        request = urllib2.Request(url+MAC, headers={'User-Agent' : "API Browser"})
+        response = urllib2.urlopen( request )
+        reader = codecs.getreader("utf-8")
+        MACLookupResponse = json.load(reader(response))
+        #print type(MACLookupResponse)
 
-    print "\n"
-    
-    Counter += 1
+        if MACLookupResponse['result'].has_key('company'):
+            MACVendor =  MACLookupResponse['result']['company']
+        else:
+            MACVendor = "UNKNOWN"  
+            
+        logging.debug('Interface Vendor: %s',  MACVendor)
+        
+        
+        #Identify Open Ports on Device
+        Scan_CMD = "sudo nmap -n -PN " + DevIP + " | grep -w open | awk '{print $1}'"
+        if DevIP is "UNKNOWN":
+            DevPorts = "UNKNOWN"
+        else:
+            Scan_CMD_Output = subprocess.Popen([Scan_CMD], shell=True, stdout=subprocess.PIPE).stdout
+            DevPorts = Scan_CMD_Output.read().splitlines()
+            DevPorts = '[%s]' % ', '.join(map(str, DevPorts))
+
+        
+        logging.debug('Open Ports: %s', DevPorts)
+        
+        Counter += 1
+
+s.enter(86400, 1, do_something, (s,))
+s.run()
--- batch/NetScan.py
+++ batch/NetScan.py
@@ -9,6 +9,15 @@ import pprint
 import subprocess
 import os
 from ConfigParser import SafeConfigParser
+import sched
+import time
+import datetime
+import logging
+
+
+#Setup logging
+logging.basicConfig(filename='../logs/NetScan.log',level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
 
 #Import varibales from NetMan.conf
 Config = SafeConfigParser()
@@ -17,55 +26,50 @@ Config.read('../conf/NetMan.conf')
 #Set CIDR variable from conf file
 CIDR =  Config.get('config', 'network')
 
-
-#Define subnet to scan
-#CIDR = "192.168.0.0/24"
-
-# ntp sync
-
-
 #Setup Database Connection
 try:
-            conn_string = "host='192.168.0.44' dbname='netman' user='netman' password='netman'"
+            conn_string = "host='localhost' port='5432' dbname='netman' user='netman' password='netman'"
             conn = psycopg2.connect(conn_string)
-            print "SUCCESS: Database Connection Established"
+            logging.info('SUCCESS: Database Connection Established')
 except:
-            print "ERROR: I am unable to connect to the database"
+            logging.warning('ERROR: I am unable to connect to the database')
 
 cursor = conn.cursor()
 
-#Execute a nmap IP layer scan to populate arp table
-NMAP_CMD = "/usr/bin/nmap -sP " + CIDR
-FNULL = open(os.devnull, 'w')
-subprocess.call([NMAP_CMD], shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-print "SUCCESS: nmap executed"
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc): 
+    s.enter(600, 1, do_something, (sc,))
 
-#Print arp table and extract MAC Addresses
+    logging.info('SUCCESS: Scheduler starting')
+  
+    #Execute a nmap IP layer scan to populate arp table
+    NMAP_CMD = "/usr/bin/nmap -sP " + CIDR
+    FNULL = open(os.devnull, 'w')
+    subprocess.call([NMAP_CMD], shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    logging.info('SUCCESS: nmap executed')
 
-MACs = subprocess.Popen(["/usr/sbin/arp -a | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"], shell=True, stdout=subprocess.PIPE).stdout
-MAClist = MACs.read().splitlines()
-print "SUCCESS: MAC Addresses Obtained from arp table"
-#print "DEBUG: Print MAC List"
-#print MAClist
+    MACs = subprocess.Popen(["/usr/sbin/arp -a | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'"], shell=True, stdout=subprocess.PIPE).stdout
+    MAClist = MACs.read().splitlines()
+    logging.info('SUCCESS: MAC Addresses Obtained from arp table')
+    
 
-#Insert MAC Addresses into DB NetScan Table
+    #Insert MAC Addresses into DB NetScan Table
 
-MAClistLength = len(MAClist)
-Counter = 0
+    MAClistLength = len(MAClist)
+    Counter = 0
 
-Counter < MAClistLength
-while (Counter < MAClistLength):
-    MAC = MAClist[Counter]
-    query = """ INSERT INTO "NetScan" ("MAC") values ('""" + MAC  + """'); """
-    cursor.execute(query)
-    Counter += 1
+    Counter < MAClistLength
+    while (Counter < MAClistLength):
+        MAC = MAClist[Counter]
+        query = """ INSERT INTO "NetScan" ("MAC") values ('""" + MAC  + """'); """
+        cursor.execute(query)
+        Counter += 1
 
-#Commit data to database
-conn.commit()
+    #Commit data to database
+    conn.commit()
+    MAClistLength = str(MAClistLength)
+    logging.info('SUCCESS: Inserting %s rows into database', MAClistLength)
 
-#DEBUG - PRINT NetScan Table
-# execute our Query
-# cursor.execute('select * from "NetScan"')
-# retrieve the records from the database
-# records = cursor.fetchall()
-# pprint.pprint(records)
+s.enter(600, 1, do_something, (s,))
+s.run()
+
